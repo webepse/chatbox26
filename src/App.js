02c4cd7b4767f7b3c2adc33bd557fda3a4234850
Boderio <berti@epse.be>
2025-11-19 12:00:15 +0100

    cours 4 - Chatbox - gestion des messages V1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Message from './components/Message';
 import Formulaire from './components/Formulaire';
+import { useParams } from "react-router-dom";
 
 function App() {
-  return (
+    let { login } = useParams();
+    const [pseudo, setPseudo] = useState(login);
+    const [messages, setMessages] = useState({})
+
+    const addMessage = message => {
+        // copie du state Messages en vue d'ajouter le nouveau message
+        const newMessages = {...messages}
+        newMessages[`message-${Date.now()}`] = message
+        setMessages(newMessages)
+    }
+
+    /*
+    state messages:
+    [
+        'message-1216154989' =>
+        {
+            pseudo: 'user',
+            message: text
+        },
+        message-1216154990 =>
+        {
+            pseudo: 'bot',
+            message: text
+        }
+     ]
+     */
+    return (
     <div className="box">
          <div>
              <div className="messages">
@@ -13,7 +40,10 @@ function App() {
                  <Message />
              </div>
          </div>
-         <Formulaire />
+         <Formulaire
+            pseudo={pseudo}
+            addMessage={addMessage}
+         />
     </div>
   );
 }
